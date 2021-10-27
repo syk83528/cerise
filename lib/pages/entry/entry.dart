0e3f066a826a5223d648c60e5b05e1475d8fb444
ggdream <gdream@yeah.net>
2021-10-27 13:46:56 +0800

    Remove entry page textField cursor when op others

--- lib/pages/entry/entry.dart
+++ lib/pages/entry/entry.dart
@@ -1,9 +1,9 @@
-import 'package:cerise/widgets/loading/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:cerise/router/router.dart';
 import 'package:cerise/tools/git/git.dart';
+import 'package:cerise/widgets/loading/loading.dart';
 
 class EntryPage extends StatelessWidget {
   EntryPage({Key? key}) : super(key: key);
@@ -15,48 +15,66 @@ class EntryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: bodyView(),
+    );
+  }
+
+  Widget bodyView() {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () => Focus.of(Get.context!).requestFocus(FocusNode()),
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: context.isPhone ? null : 240,
-              padding:
-                  EdgeInsets.symmetric(horizontal: context.isPhone ? 48 : 0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  _textField(controller: _controller1, hintText: '用户'),
-                  SizedBox(height: 12),
-                  _textField(controller: _controller2, hintText: '仓库'),
-                  SizedBox(height: 12),
-                  _textField(controller: _controller3, hintText: '凭证'),
-                ],
-              ),
-            ),
+            inputView(),
             SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  Loading.show('请求中');
-                  await Git.init(
-                    owner: _controller1.text.isEmpty ? null : _controller1.text,
-                    repo: _controller2.text.isEmpty ? null : _controller2.text,
-                    token: _controller3.text.isEmpty ? null : _controller3.text,
-                  );
-                  await Loading.close();
-                } catch (e) {
-                  final snackBar = SnackBar(content: Text(e.toString()));
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  return;
-                }
-
-                await Get.offAllNamed(RoutesNamespace.home);
-              },
-              child: Text('确认'),
-            ),
+            reqBtnView(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget reqBtnView() {
+    return ElevatedButton(
+      onPressed: () async {
+        Focus.of(Get.context!).requestFocus(FocusNode());
+
+        try {
+          Loading.show('请求中');
+          await Git.init(
+            owner: _controller1.text.isEmpty ? null : _controller1.text,
+            repo: _controller2.text.isEmpty ? null : _controller2.text,
+            token: _controller3.text.isEmpty ? null : _controller3.text,
+          );
+          await Loading.close();
+        } catch (e) {
+          await Loading.close();
+          final snackBar = SnackBar(content: Text(e.toString()));
+          ScaffoldMessenger.of(Get.context!).showSnackBar(snackBar);
+          return;
+        }
+
+        await Get.offAllNamed(RoutesNamespace.home);
+      },
+      child: Text('确认'),
+    );
+  }
+
+  Widget inputView() {
+    return Container(
+      width: Get.context!.isPhone ? null : 240,
+      padding: EdgeInsets.symmetric(horizontal: Get.context!.isPhone ? 48 : 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _textField(controller: _controller1, hintText: '用户'),
+          SizedBox(height: 12),
+          _textField(controller: _controller2, hintText: '仓库'),
+          SizedBox(height: 12),
+          _textField(controller: _controller3, hintText: '凭证'),
+        ],
       ),
     );
   }
@@ -75,15 +93,14 @@ class EntryPage extends StatelessWidget {
         hintStyle: TextStyle(fontWeight: FontWeight.bold),
         contentPadding:
             const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32),
-          borderSide: BorderSide.none,
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32),
-          borderSide: BorderSide.none,
-        ),
+        enabledBorder: _outlineInputBorder,
+        focusedBorder: _outlineInputBorder,
       ),
     );
   }
+
+  final _outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(32),
+    borderSide: BorderSide.none,
+  );
 }
